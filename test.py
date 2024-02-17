a63288c398f787cf061ceaa850a3204c7baa3e0b
unknown <risafox9@gmail.com>
2024-02-16 19:07:46 -0800

    test modification

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import main
 
 def test_add():
-    assert(main.add(3, 5), 8)
-    assert(main.add(0, 0), 0)
-    assert(main.add(-1, -1), -2)
+    assert main.add(3, 5) == 8
+    assert main.add(0, 0) == 0
+    assert main.add(-1, -1) == -2
 
 if __name__ == "__main__":
     print("hello from test")
